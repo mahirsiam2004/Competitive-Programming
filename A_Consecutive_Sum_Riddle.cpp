6bec87a6cd9be1b64a1c42ca0f7b70922241f254
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-03-07 21:59:31 +0600

    c++

---
+++ A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+int t;
+cin>>t;
+while(t--){
+
+    ll n;
+    cin >> n;
+    
+    ll sum = 0, l = 0, r = 0;
+    
+    while (sum < n) {
+        r++;
+        sum += r;
+    }
+    
+    while (sum > n) {
+        sum -= l;
+        l++;
+    }
+    
+    cout << l << " " << r << endl;
+    
+}
+return 0;
+}
