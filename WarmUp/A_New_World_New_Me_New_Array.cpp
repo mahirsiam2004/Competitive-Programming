9caf4e9bafb908aff6f5283fc3ce2b2c7de4a1a0
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-03-17 19:16:10 +0600

    added random code

---
+++ WarmUp/A_New_World_New_Me_New_Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, p;
+        cin >> n >> k >> p;
+        if (k < -n * p || k > n * p) {
+            cout << -1 <<endl;
+            continue;
+        }
+        int min_ops = abs(k) / p;
+        if (abs(k) % p != 0) {
+            min_ops++;
+        }
+        cout << min_ops << "\n";
+    }
+    return 0;
+}
