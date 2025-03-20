7a197580916eb4de48a0fdf1138c8fd71a49e88d
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-03-21 04:30:44 +0600

    added code

---
+++ WarmUp/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    int n;
+    cin >> n;
+    
+    while (n--) {
+        string word;
+        cin >> word;
+        
+        if (word.length() > 10) {
+            cout << word[0] << word.length() - 2 << word.back() << endl;
+        } else {
+            cout << word << endl;
+        }
+    }
+    return 0;
+}
