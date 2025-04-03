7387d39248ccf41e886ff32472878c7ad3d40649
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-03 14:02:00 +0600

    not ready

---
+++ WarmUp/A_Serval_and_String_Theory.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        
+        if (s < rev_s) {
+            cout << "YES" << endl;
+            continue;
+        }
+        
+        if (k == 0) {
+            cout << "NO" << endl;
+            continue;
+        }
+        
+        if (n == 1) {
+            cout << "NO" << endl;
+            continue;
+        }
+        
+        cout << "YES" << endl;
+    }
+    return 0;
+}
