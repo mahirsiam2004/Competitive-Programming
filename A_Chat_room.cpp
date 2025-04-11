00674fb5c38a472673fb23e84c230a89d8a0a77d
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-11 23:12:19 +0600

    added a chat room

---
+++ A_Chat_room.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    char str1[101];
+    char str2[101] = "hello";
+    scanf("%s", &str1);
+    int i, j = 0, count = 0;
+    int len = strlen(str1);
+    for (i = 0; i < len; i++)
+    {
+        if (str1[i] == str2[j])
+        {
+            j++;
+            count++;
+        }
+    }
+    if (count == 5)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
