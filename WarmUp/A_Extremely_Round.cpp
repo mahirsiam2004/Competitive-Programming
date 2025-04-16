b69e4f1cf09a5b78e98c62e9e0ac203ebfc4ead1
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-16 14:46:53 +0600

    added extream

--- WarmUp/A_Extremely_Round.cpp
+++ WarmUp/A_Extremely_Round.cpp
@@ -1,31 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define ll long long //note solved 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-      bool flag = false;
-      int n;
-      cin>>n;
-      map<string, int> mp;
-      string s;
-      cin>>s;
-      string a;
-      for(int i = 1; i < n; i++){
-        a = "";
-        a += s[i-1];
-        a += s[i];
-        if(mp.find(a) != mp.end() && mp[a] < i-1){
-          flag = true;
-          break;
-        }
-        else if(mp.find(a) == mp.end()){
-          mp[a] = i;
-        }
-  
-      }
-      if(flag) cout<<"YES"<<endl; else cout<<"NO"<<endl;
-    }
+   
     return 0;
 }
