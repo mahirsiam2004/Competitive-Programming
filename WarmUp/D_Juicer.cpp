d996990d81a7ee25e4874a32c5eb01e0cccfb33b
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-04-27 14:57:01 +0600

    added jucier

---
+++ WarmUp/D_Juicer.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    int n, b, d;
+    cin >> n >> b >> d;
+    int waste = 0, emptyCount = 0;
+    for (int i = 0; i < n; ++i) {
+        int orange;
+        cin >> orange;
+        if (orange <= b) {
+            waste += orange;
+            if (waste > d) {
+                emptyCount++;
+                waste = 0;
+            }
+        }
+    }
+    cout << emptyCount << endl;
+    return 0;
+}
