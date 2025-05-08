9753c16e52291516b96bd0b6bf08adf1a540898b
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-05-08 17:10:27 +0600

    fixed a boy or girl problem

--- WarmUp/A_Boy_or_Girl.cpp
+++ WarmUp/A_Boy_or_Girl.cpp
@@ -1,22 +1,46 @@
-#include<bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     string s;
+//     cin >> s;
+    
+//     set<char> distinct_letters;
+//     for (char c : s) {
+//         distinct_letters.insert(c);
+//     }
+    
+//     if (distinct_letters.size() % 2 == 0) {
+//         cout << "CHAT WITH HER!" << endl;
+//     } else {
+//         cout << "IGNORE HIM!" << endl;
+//     }
+
+//     return 0;
+// }
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-int main(){
+
+int main() {
     string s;
     cin >> s;
-    vector<int>freq{26,0};
-    int count=0;
-    for(int c:s){
-        if(freq[c-'a'] == 0){
+
+    vector<int> freq(26, 0);
+
+    int count = 0;
+
+    for (char c : s) {
+        if (freq[c - 'a'] == 0) {
             count++;
+            freq[c - 'a'] = 1;
         }
     }
-    if((s.size()-count)%2 == 0){
-        cout<<"CHAT WITH HER!"<<endl;
-    }else{
-        cout<<"IGNORE HIM!"<<endl;
-    }
 
+    if (count % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    } else {
+        cout << "IGNORE HIM!" << endl;
+    }
 
     return 0;
 }
